51fd87ff4695cce670b1a87d453214151e325be7
sreepathy s prabhu <sreepathysp@gmail.com>
2024-03-09 15:40:34 +0530

    Initial

---
+++ ShoppingManagement/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return ["value1", "value2"];
+        }
+    }
+}
